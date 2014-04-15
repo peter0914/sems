5ba257b1455df6be54438198bcdcfe498bbc7a24
phuking <gjsdnd123@nate.com>
2014-04-15 17:53:36 +0900

    .

--- sems/src/cours/CoursDao.java
+++ sems/src/cours/CoursDao.java
@@ -1,5 +1,11 @@
 package cours;
 
-public class CoursDao {
+import java.util.List;
 
+public interface CoursDao {
+	public void insert(CoursVo cours) throws Throwable;
+	public List<CoursVo> list (int pageNo, int pageSize) throws Throwable;
+	public CoursVo detail(int no) throws Throwable;
+	public int update(CoursVo cours) throws Throwable;
+	public int delete(int no) throws Throwable;
 }
--- sems/src/cours/MysqlCoursDao.java
+++ sems/src/cours/MysqlCoursDao.java
@@ -1,5 +1,32 @@
 package cours;
 
-public class MysqlCoursDao {
+import java.util.List;
+
+public class MysqlCoursDao implements CoursDao{
+
+	@Override
+	public void insert(CoursVo cours) throws Throwable {
+		
+	}
+
+	@Override
+	public List<CoursVo> list(int pageNo, int pageSize) throws Throwable {
+		return null;
+	}
+
+	@Override
+	public CoursVo detail(int no) throws Throwable {
+		return null;
+	}
+
+	@Override
+	public int update(CoursVo cours) throws Throwable {
+		return 0;
+	}
+
+	@Override
+	public int delete(int no) throws Throwable {
+		return 0;
+	}
 
 }
